9988dfa8054b0c5b38bb4af6cbab21aebf8d6554
Prathameshwarghane <101662451+Prathameshwarghane@users.noreply.github.com>
2022-10-26 11:17:15 +0530

    QuizGame

--- QuizGame.py
+++ QuizGame.py
@@ -1,26 +1,40 @@
-def check_guess(guess, answer):
-    global score
-    still_guessing = True
-    attempt = 0
-    while still_guessing and attempt < 3:
-        if guess.lower() == answer.lower():
-            print("Correct Answer")
-            score = score + 1
-            still_guessing = False
-        else:
-            if attempt < 2:
-                guess = input("Sorry Wrong Answer, try again")
-            attempt = attempt + 1
-    if attempt == 3:
-        print("The Correct answer is ",answer )
-    
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
 score = 0
-print("Simple Anime Quiz ")
-print("")
-guess1 = input("1.Haikyu!! is an anime centred around a boy who wants to be successful in which sport?")
-check_guess(guess1, "Volleyball")
-guess2 = input("2.In the first season of In Sword Art Online, what is the black blade used by Kirito called? ")
-check_guess(guess2, "The Elucidator")
-guess3 = input("3.As of 2021, what is the highest-grossing anime movies of all time?")
-check_guess(guess3, "Demon Slayer: Mugen Train")
-print("Your Score is "+ str(score))
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
